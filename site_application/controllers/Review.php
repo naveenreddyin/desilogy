9d81e2d99216d60935b2d2c89bd2e475822f54ce
naveen <naveen>
2016-05-10 13:45:55 +0200

    uploading is working

--- site_application/controllers/Review.php
+++ site_application/controllers/Review.php
@@ -44,12 +44,45 @@ class Review extends CI_Controller {
 	}
 
 	public function create_review_test(){
+		$upload_path = realpath(APPPATH . '../public/uploads');
+
+		$config['upload_path'] = $upload_path;
+		$config['allowed_types'] = 'gif|jpg|png';
+		$config['max_size']	= '500';
+		$config['max_width']  = '1024';
+		$config['max_height']  = '768';
+		$config['overwrite'] = FALSE;
+
+		$this->load->library('upload', $config);
+		
+		$files = $_FILES;
+
 		// print_r($_FILES);
 		$count = count($_FILES['images']['name']);
+		$data_arr = array();
 
 		for($i = 0; $i < $count; $i++){
-			if($_FILES['images']['name'][$i])
-				echo $_FILES['images']['name'][$i];
+			if($files['images']['name'][$i])
+			{
+				$_FILES['images']['name']= $files['images']['name'][$i];
+				// echo $_FILES['images']['name'].'</br>';
+		        $_FILES['images']['type']= $files['images']['type'][$i];
+		        $_FILES['images']['tmp_name']= $files['images']['tmp_name'][$i];
+		        $_FILES['images']['error']= $files['images']['error'][$i];
+		        $_FILES['images']['size']= $files['images']['size'][$i];
+
+		        $this->upload->initialize($config);
+
+		        $this->upload->do_upload('images');
+
+		        echo $this->upload->display_errors();
+		        $data = $this->upload->data();
+		       $data_arr['path'] = $data['full_path'];
+
+		        // print_r($data['full_path']);
+			}
+
+			print_r($data_arr);
 			// else
 			// 	echo 'no';
 		}

b43a609a0fbac5f25c061936803b00f9a10721be
naveenreddyin <naveenreddyin@gmail.com>
2016-05-11 07:44:49 +0200

    made changes to browse restro

--- site_application/views/restaurant/browse.php
+++ site_application/views/restaurant/browse.php
@@ -9,23 +9,11 @@ Logo
 	<h1><?= $query->name; ?></h1>
 	<div class="row">
 		<div class="col-md-8">
-		<div>
-
-  <!-- Nav tabs -->
-  <ul class="nav nav-tabs" role="tablist">
-    <li role="presentation" class="active"><a href="#home" aria-controls="home" role="tab" data-toggle="tab">Home</a></li>
-    <li role="presentation"><a href="#profile" aria-controls="profile" role="tab" data-toggle="tab">Profile</a></li>
-    <li role="presentation"><a href="#messages" aria-controls="messages" role="tab" data-toggle="tab">Messages</a></li>
-  </ul>
-
-  <!-- Tab panes -->
-  <div class="tab-content">
-    <div role="tabpanel" class="tab-pane active" id="home">home</div>
-    <div role="tabpanel" class="tab-pane" id="profile">profile</div>
-    <div role="tabpanel" class="tab-pane" id="messages">message</div>
-  </div>
-
-</div>
+		<ul class="nav nav-pills">
+  <li role="presentation" class="active"><a href="#">Home</a></li>
+  <li role="presentation"><a href="#">Profile</a></li>
+  <li role="presentation"><a href="#">Messages</a></li>
+</ul>
 			<div class="short-text">
 				<?= $query->body; ?>
 			</div>
